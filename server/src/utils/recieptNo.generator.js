133c846c06e782f8976a22697b3d8e9d7a089148
Joel Ababio Dartey <joeldartey71@gmail.com>
2026-09-02 20:49:34 +0100

    reciw

--- server/src/utils/recieptNo.generator.js
+++ server/src/utils/recieptNo.generator.js
@@ -4,7 +4,7 @@ const recieptNumberGenerator = () => {
   const date = new Date().toISOString().slice(0, 10).replaceAll("-", "");
   const randomCode = crypto.randomBytes(6).toString("hex").toUpperCase();
 
-  return `SMSWINNER-${date}-${randomCode}`;
+  return `WaveVerify-${date}-${randomCode}`;
 };
 
 export default recieptNumberGenerator;
